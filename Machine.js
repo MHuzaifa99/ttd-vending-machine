4710456fd481ea2142105900adee23cd782f886a
Muhammad Huzaifa <muhammadhuzaifa457@gmail.com>
2023-04-13 02:43:21 +0500

    all test cases passed

--- Machine.js
+++ Machine.js
@@ -16,4 +16,64 @@ module.exports = class Machine {
       return `You have deposited Rs ${this.totalDeposit}`;
     }
   }
+  selectItem(code) {
+    let check = false
+    let changeReturn = [20, 10]
+    for (let i in this.items) {
+      if (code == i) {
+        check = true
+        break
+      }
+    }
+    if (check) {
+      if (this.items[code].price > this.totalDeposit) {
+        return `Your deposit is insufficient.  Please add Rs ${this.items[code].price - this.totalDeposit} for this item`
+      }
+      if (this.items[code].price <= this.totalDeposit) {
+        let change = this.totalDeposit - this.items[code].price
+        change = this.changeFun(change)
+        if (JSON.stringify(change) === JSON.stringify(changeReturn)) {
+          return {
+            item: this.items[code].name,
+            change: change == [] ? [] : change
+        }
+        }
+        return 'Cannot return proper change. Please choose another item or cancel the transaction'
+      }
+
+    }
+    return "The item you selected is unavailable";
+  }
+
+  cancel() {
+    return { change: this.changeFun(this.totalDeposit) }
+  }
+
+  changeFun(change) {
+    let changeArr = []
+
+    if (change >= 100) {
+      change = change - 100
+      console.log(change)
+      changeArr.push(100)
+      this.changeFun(change)
+    }
+    if (change >= 50) {
+      console.log({ change })
+      change = change - 50
+      changeArr.push(50)
+      this.changeFun(change)
+    }
+    if (change >= 20) {
+      change = change - 20
+      changeArr.push(20)
+      this.changeFun(change)
+    }
+    if (change >= 10) {
+      change = change - 10
+      changeArr.push(10)
+      this.changeFun(change)
+    }
+    return changeArr
+  }
 }
--- Machine.test.js
+++ Machine.test.js
@@ -52,3 +52,93 @@ test("depositing money", () => {
     //Assert
     expect(machine.deposit(50)).to.be.equal(result);
   });
+
+  //AC 4:As a customer, I want to see a message if my item is unavailable, so that I can make another choice.
+  
+  // Given I am using the vending machine,
+  // when I enter a code for an item that is unavailable,
+  // then I see a message that the item is unavailable.
+  // selectItem(code) returns 'The item you selected is unavailable'
+  
+  test("unavailable item msg", () => {
+    //Arrange
+    const machine = new Machine();
+    const result = "The item you selected is unavailable";
+  
+    //Assert
+    expect(machine.selectItem(3)).to.be.equal(result);
+  });
+  
+  // AC 5: As a customer, I want to see a message if my deposit is insufficient, so that I know to add more money.
+  
+  // Given I have made a choice,
+  // when I have not deposited enough money for that item,
+  // then I see a message telling me how much more to deposit.
+  // selectItem(code) returns 'Your deposit is insufficient.  Please add Rs 20 for this item'
+  
+  test("insufficient deposit msg", () => {
+    //Arrange
+    const machine = new Machine();
+    const result =
+      "Your deposit is insufficient.  Please add Rs 20 for this item";
+    //Act
+    machine.deposit(50);
+    //Assert
+    expect(machine.selectItem(2)).to.be.equal(result);
+  });
+  
+  // As a customer, I want to receive change, so that I don’t pay more than the item costs.
+  // Given I have made a selection,
+  // when the item is delivered,
+  // then I receive correct change (in correct monetary units)
+  // selectItem(code) returns an object with the item and an array of bills {item: 'mints', change: [20, 10]}
+  
+  test("return the change", () => {
+    // Arrange
+    const machine = new Machine()
+    const expected = {
+      item: "mints",
+      change: [20, 10]
+    }
+    // Act
+    machine.deposit(100);
+    const result = machine.selectItem(2)
+    // Assert
+    expect(expected).to.deep.equal(result)
+  })
+  
+  //AC6:  As a customer, I want to receive my money back when I push the cancel button, 
+  // so that I can change my mind.
+  // Given that I have deposited money,
+  // When I push the cancel button,
+  // Then I receive my money back
+  // cancel() returns {change: [100]}
+  
+  test("cancel button", () => {
+    //Arrange
+    const machine = new Machine();
+    const result = { change: [100] }
+    //Act
+    machine.deposit(100);
+    //Assert
+    expect(machine.cancel()).to.deep.equal(result);
+  });
+  
+  // As a customer, I want to know if the vending machine can make change, 
+  // so that I can cancel my choice if it can't make change.
+  // Given I have deposited money and selected a choice,
+  // when the machine does not have correct change,
+  // then I see a message
+  // selectItem(code) returns 'Cannot return proper change. 
+  // Please choose another item or cancel the transaction'
+  
+  test("can not return change properly", () => {
+    // Arrange
+    const machine = new Machine()
+    const expected = 'Cannot return proper change. Please choose another item or cancel the transaction'
+    // Act
+    machine.deposit(500)
+    const result = machine.selectItem(1)
+    // Assert
+    expect(expected).to.deep.equal(result)
+  })
